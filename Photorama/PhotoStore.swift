186e25170148458dd8569291112bc40c6b77b141
Bogdan Orzea <bogdanorzea@users.noreply.github.com>
2019-03-17 10:58:54 -0400

    Chapter21 (#2)
    * Adds UICollectionView and dummy UICollectionViewCell
    
    * Complete Chapter 21

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -23,6 +23,8 @@ enum PhotoResult {
 }
 
 class PhotoStore {
+    var imageStore = ImageStore()
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -51,6 +53,15 @@ class PhotoStore {
     }
     
     func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        
+        let photoKey = photo.photoID
+        if let image = imageStore.image(fromKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+        
         let photoURL = photo.remoteURL
         let request = URLRequest(url: photoURL)
         
@@ -58,6 +69,11 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processImageRequest(data: data, error: error)
+            
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
+            
             OperationQueue.main.addOperation {
                 completion(result)
             }
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
-    var store: PhotoStore!
+class PhotosViewController: UIViewController, UICollectionViewDelegate {
+    @IBOutlet var collectionView: UICollectionView!
     
+    var store: PhotoStore!
+    let photoDataSource = PhotoDataSource()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = photoDataSource
+        collectionView.delegate = self
         
         store.fetchInterestingPhotos {
             (photosResult) -> Void in
@@ -21,25 +26,44 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
-                if let firstPhoto = photos.first {
-                    self.updateImageView(for: firstPhoto)
-                }
+                self.photoDataSource.photos = photos
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
+                self.photoDataSource.photos.removeAll()
+            }
+            
+            self.collectionView.reloadSections(IndexSet(integer: 0))
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let photo = photoDataSource.photos[indexPath.row]
+        
+        store.fetchImage(for: photo) { (result) -> Void in
+            guard let photoIndex = self.photoDataSource.photos.index(of: photo),
+                case let .success(image) = result else {
+                return
+            }
+            let photoIndexPath = IndexPath(item: photoIndex, section: 0)
+            
+            if let cell = self.collectionView.cellForItem(at: photoIndexPath) as? PhotoCollectionViewCell {
+                cell.update(with: image)
             }
         }
     }
     
-    func updateImageView(for photo: Photo) {
-        store.fetchImage(for: photo) {
-            (imageResult) -> Void in
-            
-            switch imageResult {
-            case let .success(image):
-                self.imageView.image = image
-            case let .failure(error):
-                print("Error downloading image: \(error)")
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showPhoto":
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                let photo = photoDataSource.photos[selectedIndexPath.row]
+                let destinationVC = segue.destination as! PhotoInfoViewController
+                
+                destinationVC.photo = photo
+                destinationVC.store = store
             }
+        default:
+            preconditionFailure("Unexpected segue identifier")
         }
     }
 }
